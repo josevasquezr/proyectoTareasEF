a518a8d72e9774cf7d7d306be9730087cff912cd
jvasquez <javierv.ramos@gmail.com>
2022-05-21 13:23:27 -0600

    Creacion de modelos y conexion a DB usando FluentAPI

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -5,21 +5,21 @@ namespace proyectoTareasEF.Models
 {
     public class Tarea
     {
-        [Key]
+        //[Key]
         public Guid TareaId { get; set; }
 
-        [ForeignKey("CategoriaId")]
+        //[ForeignKey("CategoriaId")]
         public Guid CategoriaId { get; set; }
 
-        [Required]
-        [MaxLength(200)]
+        //[Required]
+        //[MaxLength(200)]
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public Prioridad PrioridadTarea { get; set; }
         public DateTime FechaCreacion { get; set; }
         public virtual Categoria Categoria { get; set; }
         
-        [NotMapped]
+        //[NotMapped]
         public string Resumen { get; set; }
 
     }
