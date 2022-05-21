b433a3920fa1956cdc23c8361bcb0a20a1f400a2
jvasquez <javierv.ramos@gmail.com>
2022-05-20 23:20:08 -0600

    Creacion de base de datos en sql server

--- Program.cs
+++ Program.cs
@@ -5,7 +5,9 @@ using proyectoTareasEF;
 var builder = WebApplication.CreateBuilder(args);
 
 // Conexion a base de datos
-builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+//builder.Services.AddDbContext<TareasContext>(p => p.UseInMemoryDatabase("TareasDB"));
+
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("TareasDBConexion"));
 
 var app = builder.Build();
 
@@ -13,11 +15,9 @@ app.MapGet("/", () => "Hello World!");
 
 //Confirmacion de creacion de base de datos
 app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) => {
-    if(dbContext.Database.EnsureCreated()){
-        return Results.Ok($"Base de datos en memoria: { dbContext.Database.IsInMemory() }");
-    }else{
-        return Results.Problem("Error al crear base de datos InMemory");
-    }
+    dbContext.Database.EnsureCreated();
+    //return Results.Ok($"Base de datos en memoria: { dbContext.Database.IsInMemory() }");
+    return Results.Ok($"Base de datos en SqlServer2019: { dbContext.Database.IsSqlServer() }");
 });
 
 app.Run();
