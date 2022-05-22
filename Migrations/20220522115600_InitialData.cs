62587838a3851f5186beaa9e0573a02f3f9bcabf
jvasquez <javierv.ramos@gmail.com>
2022-05-22 10:44:38 -0600

    Siembra de datos InitialData

---
+++ Migrations/20220522115600_InitialData.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proyectoTareasEF.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Categoria",
+                columns: new[] { "CategoriaId", "Descripcion", "Nombre", "Peso" },
+                values: new object[,]
+                {
+                    { new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac23"), null, "Actividades Laborales", 50 },
+                    { new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac24"), null, "Actividades Personales", 30 },
+                    { new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac25"), null, "Actividades Familiares", 20 },
+                    { new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac26"), null, "Servicios Públicos", 80 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tarea",
+                columns: new[] { "TareaId", "CategoriaId", "Descripcion", "FechaCreacion", "FechaHoraRecordatorio", "PrioridadTarea", "Recordatorio", "Titulo" },
+                values: new object[,]
+                {
+                    { new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac27"), new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac23"), null, new DateTime(2022, 5, 22, 5, 56, 0, 39, DateTimeKind.Local).AddTicks(429), null, 1, false, "Revisión de documentación SIISAR" },
+                    { new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac28"), new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac24"), null, new DateTime(2022, 5, 22, 5, 56, 0, 39, DateTimeKind.Local).AddTicks(450), null, 0, false, "Ver pelicula Batman en HBO" },
+                    { new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac29"), new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac25"), null, new DateTime(2022, 5, 22, 5, 56, 0, 39, DateTimeKind.Local).AddTicks(454), new DateTime(2022, 12, 1, 17, 0, 0, 0, DateTimeKind.Unspecified), 0, true, "Viaje Roatán" },
+                    { new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac30"), new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac26"), null, new DateTime(2022, 5, 22, 5, 56, 0, 39, DateTimeKind.Local).AddTicks(562), new DateTime(2022, 5, 29, 14, 0, 0, 0, DateTimeKind.Unspecified), 2, true, "Pago de energia eléctrica" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac27"));
+
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac28"));
+
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac29"));
+
+            migrationBuilder.DeleteData(
+                table: "Tarea",
+                keyColumn: "TareaId",
+                keyValue: new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac30"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac23"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac24"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac25"));
+
+            migrationBuilder.DeleteData(
+                table: "Categoria",
+                keyColumn: "CategoriaId",
+                keyValue: new Guid("c88ff4bc-6a99-48ee-95f5-848fc205ac26"));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Tarea",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- TareasContext.cs
+++ TareasContext.cs
@@ -11,12 +11,16 @@ namespace proyectoTareasEF
         public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
+            List<Categoria> categorias = this.getCategorias();
+            List<Tarea> tareas = this.getTareas();
+
             modelBuilder.Entity<Categoria>(categoria => {
                 categoria.ToTable("Categoria");
                 categoria.HasKey(p => p.CategoriaId);
                 categoria.Property(p => p.Nombre).IsRequired().HasMaxLength(150);
-                categoria.Property(p => p.Descripcion);
+                categoria.Property(p => p.Descripcion).IsRequired(false);
                 categoria.Property(p => p.Peso);
+                categoria.HasData(categorias);
             });
 
             modelBuilder.Entity<Tarea>(tarea => {
@@ -24,13 +28,83 @@ namespace proyectoTareasEF
                 tarea.HasKey(p => p.TareaId);
                 tarea.HasOne(p => p.Categoria).WithMany(p => p.Tareas).HasForeignKey(p => p.CategoriaId);
                 tarea.Property(p => p.Titulo).IsRequired().HasMaxLength(200);
-                tarea.Property(p => p.Descripcion);
+                tarea.Property(p => p.Descripcion).IsRequired(false);
                 tarea.Property(p => p.PrioridadTarea);
                 tarea.Property(p => p.FechaCreacion);
                 tarea.Property(p => p.Recordatorio);
                 tarea.Property(p => p.FechaHoraRecordatorio);
                 tarea.Ignore(p => p.Resumen);
+                tarea.HasData(tareas);
             });
+        }
+
+        private List<Categoria> getCategorias(){
+            List<Categoria> categorias = new List<Categoria>(){
+                new Categoria(){
+                    CategoriaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac23"),
+                    Nombre = "Actividades Laborales",
+                    Peso = 50
+                },
+                new Categoria(){
+                    CategoriaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac24"),
+                    Nombre = "Actividades Personales",
+                    Peso = 30
+                },
+                new Categoria(){
+                    CategoriaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac25"),
+                    Nombre = "Actividades Familiares",
+                    Peso = 20
+                },
+                new Categoria(){
+                    CategoriaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac26"),
+                    Nombre = "Servicios Públicos",
+                    Peso = 80
+                }
+            }; 
+
+            return categorias;
+        }
+
+        private List<Tarea> getTareas()
+        {
+            List<Tarea> tareas = new List<Tarea>(){
+                new Tarea(){
+                    TareaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac27"),
+                    CategoriaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac23"),
+                    Titulo = "Revisión de documentación SIISAR",
+                    PrioridadTarea = Prioridad.Media,
+                    FechaCreacion = DateTime.Now,
+                    Recordatorio = false
+                },
+                new Tarea(){
+                    TareaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac28"),
+                    CategoriaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac24"),
+                    Titulo = "Ver pelicula Batman en HBO",
+                    PrioridadTarea = Prioridad.Baja,
+                    FechaCreacion = DateTime.Now,
+                    Recordatorio = false
+                },
+                new Tarea(){
+                    TareaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac29"),
+                    CategoriaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac25"),
+                    Titulo = "Viaje Roatán",
+                    PrioridadTarea = Prioridad.Baja,
+                    FechaCreacion = DateTime.Now,
+                    Recordatorio = true,
+                    FechaHoraRecordatorio = DateTime.Parse("01-12-2022 17:00:00")
+                },
+                new Tarea(){
+                    TareaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac30"),
+                    CategoriaId = Guid.Parse("c88ff4bc-6a99-48ee-95f5-848fc205ac26"),
+                    Titulo = "Pago de energia eléctrica",
+                    PrioridadTarea = Prioridad.Alta,
+                    FechaCreacion = DateTime.Now,
+                    Recordatorio = true,
+                    FechaHoraRecordatorio = DateTime.Parse("29-05-2022 14:00:00")
+                }
+            };
+
+            return tareas;
         }
     }
 }
